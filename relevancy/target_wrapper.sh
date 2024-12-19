d1754e69be4ecd3627863e53a7a0a6d1570361ee
Thomas Brettin <brettin@anl.gov>
2024-12-19 17:46:24 -0600

    make targets.txt an argument

--- relevancy/target_wrapper.sh
+++ relevancy/target_wrapper.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-for n in `cat targets.txt` ; do
+TARGETS=$1
+for n in `cat ${TARGETS}` ; do
     mkdir $n ; 
     /opt/miniconda3/envs/litscan/bin/python -u /Users/brettin/github/Literature-Scan/relevancy/ls_main2.py --term $n --logfile $n/log --get_partners > $n/out
 done
