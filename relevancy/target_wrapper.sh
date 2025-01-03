7081c972af7b6f7c21b49764c53f77a23b46d141
Thomas Brettin <brettin@anl.gov>
2025-01-03 10:15:51 -0600

    fixed paths

--- relevancy/target_wrapper.sh
+++ relevancy/target_wrapper.sh
@@ -3,5 +3,5 @@
 TARGETS=$1
 for n in `cat ${TARGETS}` ; do
     mkdir $n ; 
-    /opt/miniconda3/envs/litscan/bin/python -u /Users/brettin/github/Literature-Scan/relevancy/ls_main2.py --term $n --logfile $n/log --get_partners > $n/out
+    python -u ./ls_main2.py --term $n --logfile $n/log --get_partners > $n/out
 done
