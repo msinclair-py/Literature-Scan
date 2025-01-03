679f13ce6e23eda04ad0bb081882648d184f4c1e
brettin <brettin@anl.gov>
2025-01-03 11:04:54 -0500

    clean up

--- relevancy/LLMConfig.py
+++ relevancy/LLMConfig.py
@@ -1,7 +1,8 @@
 class LLMConfig:
     """Configuration settings for LLM interactions"""
     def __init__(self):
-        self.api_key = "cmsc-35360"
+        self.api_key = "EMPTY"
         self.base_url = "http://localhost:9999/v1"
         self.model = "llama31-405b-fp8"
-        self.temperature = 0.7
+        self.temperature = 1.0 
+
