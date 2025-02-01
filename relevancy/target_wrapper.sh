1de70715ed616376e52a515bbb6c072c28f02cb4
Thomas Brettin <brettin@anl.gov>
2025-02-01 09:24:50 -0600

    set default no_delete

--- relevancy/target_wrapper.sh
+++ relevancy/target_wrapper.sh
@@ -3,5 +3,5 @@
 TARGETS=$1
 for n in `cat ${TARGETS}` ; do
     mkdir $n ; 
-    python -u ./ls_main2.py --term $n --logfile $n/log --get_partners > $n/out
+    python -u ./ls_main2.py --term $n --logfile $n/log --get_partners --no_delete > $n/out
 done
